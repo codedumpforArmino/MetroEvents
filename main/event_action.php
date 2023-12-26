20877e6a6ca601e3bd6ab7a4543f2df3baadc91b
Heather <charlottecaelestis@gmail.com>
2023-12-26 18:41:44 +0800

    Upvotes in reviews

--- main/event_action.php
+++ main/event_action.php
@@ -5,17 +5,27 @@
     $events = json_decode(file_get_contents($eventsJSON), true);
     $action = $_POST['action'];
     $redirectUrl = "mainpage.php"; // Replace with your HTML page URL
+    $reviews = json_decode(file_get_contents('../data/reviews.json'), true);
+    $userId = $_COOKIE['UserID'];
+
 
     //function to upvote
-    if($action === 'upvote'){
+    if ($action === 'upvote') {
         $eventId = $_POST['event_id'];
-        foreach($events as $index => $event){
-            if ($event['id'] == $eventId) {
-                $events[$index]['upvotes']++;
-                break;
-            }
+    
+        foreach ($reviews as $index => $review) {
+            if (!isset($review['upvoted']) || !$review['upvoted']) {
+                // Update the upvote count
+                $reviews[$index]['upvote']++;
+
+                // Set 'upvoted' flag to true
+                $reviews[$index]['upvoted'] = true;
+
+                file_put_contents('../data/reviews.json', json_encode($reviews, JSON_PRETTY_PRINT));
+
+                   break; 
+                } 
         }
-        file_put_contents('../data/events.json', json_encode($events, JSON_PRETTY_PRINT));
     }
 
     //function to add new event
